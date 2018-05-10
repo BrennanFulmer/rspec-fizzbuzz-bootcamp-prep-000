359a8aad439fa2845846132d9035731e193c5e21
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-10 15:22:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(int)
- if growth % 3 == 0 && growth % 5 == 0 
-    puts "Foobar"
+  if growth % 3 == 0 && growth % 5 == 0 
+  puts "Foobar"
   elsif growth % 3 == 0 
-    puts "Foo"
+  puts "Foo"
   elsif growth % 5 == 0
   puts "Bar"
 end
