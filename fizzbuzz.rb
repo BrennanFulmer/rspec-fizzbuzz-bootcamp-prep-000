7d1a52f5c1a4560f5a8ac3c3398d7e737a2927e9
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-10 15:24:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 
-    return "Fooba"
+    return "FizzBuzz"
   elsif int % 3 == 0 
-  puts "Foo"
+    return "Fizz"
   elsif int % 5 == 0
-  puts "Bar"
+  puts "Buzz"
 end
